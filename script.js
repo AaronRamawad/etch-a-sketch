eaa0137ef785b1e1354bd5c111c7058741bac187
TheSlumberingEgg <aramawad05@gmail.com>
2024-11-01 19:09:54 -0400

    create grid using js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function main() {
     const container = document.querySelector(".container");
 
-    
+    const GRID_SIZE = 4;
+
+    for (let i = 0; i < GRID_SIZE; i++) {
+        let gridRow = document.createElement("div");
+        gridRow.setAttribute("class", "grid-row");
+        for (let j = 0; j < GRID_SIZE; j++) {
+            let gridSquare = document.createElement("div");
+            gridSquare.setAttribute("class", "grid-square");
+            gridRow.appendChild(gridSquare);
+        }
+        container.appendChild(gridRow);
+    }
+
+
 }
+
+main();
