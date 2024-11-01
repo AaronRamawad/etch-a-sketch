facf18712d88d20b754e689cdcb406475fffb02d
TheSlumberingEgg <aramawad05@gmail.com>
2024-11-01 19:49:18 -0400

    Add input to change size

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 function main() {
     const container = document.querySelector(".container");
+    const sizeInput = document.querySelector("input");
 
-    const GRID_SIZE = 4;
+    
 
-    for (let i = 0; i < GRID_SIZE; i++) {
+    let gridSize;
+    if (sizeInput.value === "" || +sizeInput.value < 0 || +sizeInput.value > 100) {
+        gridSize = 16;
+    } else {
+        gridSize = +sizeInput.value;
+    }
+
+    for (let i = 0; i < gridSize; i++) {
         let gridRow = document.createElement("div");
         gridRow.setAttribute("class", "grid-row");
-        for (let j = 0; j < GRID_SIZE; j++) {
+        for (let j = 0; j < gridSize; j++) {
             let gridSquare = document.createElement("div");
             gridSquare.setAttribute("class", "grid-square");
+
+            gridSquare.addEventListener("mouseover", () => {
+                gridSquare.style.backgroundColor = "rgb(0,0,0)";
+            })
+
             gridRow.appendChild(gridSquare);
         }
         container.appendChild(gridRow);
     }
 
-
+    sizeInput.addEventListener("input", () => {
+        container.innerHTML = "";
+        main();
+    })
 }
 
 main();
